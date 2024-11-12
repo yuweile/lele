d945d868fdb16d41726ceade4a1b8c9e80b06cf0
yuweile <71496599+yuweile@users.noreply.github.com>
2024-11-12 16:30:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,5 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+git clone https://github.com/EOYOHOO/UA2F.git packge/UA2F
+git clone https://github.com/EOYOHOO/rkp-ipid.git packge/rkp-ipid
